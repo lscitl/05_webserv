4247a7daeb6fe599e1c9e847db5936d62ebb8b64
Seungduk Seo <seseo@student.42seoul.kr>
2022-12-10 19:43:25 +0900

    testing

--- test.cpp
+++ test.cpp
@@ -4,13 +4,50 @@
 #include <fcntl.h>
 #include <sys/uio.h>
 
-#define BUF_LEN 10
-#define IOVEC_LEN 3
+// #define BUF_LEN 10
+// #define IOVEC_CNT 4
 
-void set_rbuf_len( struct iovec *buf, ssize_t read_len, int buf_len ) {
+enum { BUF_LEN = 10, IOVEC_CNT = 4 };
+
+class ClientBuffer {
+   private:
+	struct iovec *buf;
+
+	ClientBuffer() {
+		set_buffer( IOVEC_CNT, BUF_LEN );
+	}
+	~ClientBuffer() {
+		del_buffer( buf, IOVEC_CNT );
+	}
+};
+
+struct iovec *set_buffer( int iovec_cnt, int buf_len ) {
+	struct iovec *tmp = new struct iovec[iovec_cnt];
+	for ( int i = 0; i < iovec_cnt; i++ ) {
+		tmp[i].iov_base = new char[buf_len];
+		tmp[i].iov_len = buf_len;
+	}
+	return tmp;
+}
+
+void del_buffer( struct iovec *buf, int iovec_cnt ) {
+	for ( int i = 0; i < iovec_cnt; i++ ) {
+		delete (char *)buf[i].iov_base;
+	}
+	delete buf;
+}
+
+void set_rbuf_len( struct iovec *buf, ssize_t read_len, int iovec_cnt,
+				   int buf_len ) {
 	int n = read_len / buf_len;
 	int r = read_len % buf_len;
-	buf[n].iov_len = r;
+
+	if ( r != 0 ) {
+		buf[n++].iov_len = r;
+	}
+	while ( n < iovec_cnt ) {
+		buf[n++].iov_len = 0;
+	}
 }
 
 void reset_buf_len( struct iovec *buf, int iovec_cnt, int buf_len ) {
@@ -20,34 +57,38 @@ void reset_buf_len( struct iovec *buf, int iovec_cnt, int buf_len ) {
 }
 
 int main( void ) {
-	struct iovec buf[IOVEC_LEN];
-	char         rdbuf1[BUF_LEN];
-	char         rdbuf2[BUF_LEN];
-	char         rdbuf3[BUF_LEN];
-	int          fd = open( "aaa", O_RDWR | O_NONBLOCK, 0777 );
-	int          fd2 = open( "bbb", O_RDWR | O_NONBLOCK, 0777 );
-	char         asdf[21];
+	int           fd = open( "aaa", O_RDWR | O_NONBLOCK, 0777 );
+	int           fd2 = open( "bbb", O_RDWR | O_NONBLOCK, 0777 );
+	struct iovec *buf;
 
-	reset_buf_len( buf, IOVEC_LEN, BUF_LEN );
+	buf = set_buffer( IOVEC_CNT, BUF_LEN );
 
 	int n;
-	int n2;
-	n = readv( fd, buf, 3 );
-	set_rbuf_len( buf, n, BUF_LEN );
+	n = readv( fd, buf, IOVEC_CNT );
+	set_rbuf_len( buf, n, IOVEC_CNT, BUF_LEN );
+
 	printf( "read: %lu, %lu, %lu\n", buf[0].iov_len, buf[1].iov_len,
 			buf[2].iov_len );
-	reset_buf_len( buf, IOVEC_LEN, BUF_LEN );
 	printf( "len: %d\n", n );
-	printf( "write: %lu, %lu, %lu\n", buf[0].iov_len, buf[1].iov_len,
-			buf[2].iov_len );
-	readv( fd2, buf, 3 );
-	set_rbuf_len( buf, n, BUF_LEN );
+
+	writev( STDOUT_FILENO, buf, IOVEC_CNT );
+	printf( "\n" );
+
+	reset_buf_len( buf, IOVEC_CNT, BUF_LEN );
+
+	n = readv( fd2, buf, IOVEC_CNT );
+	set_rbuf_len( buf, n, IOVEC_CNT, BUF_LEN );
+
 	printf( "read: %lu, %lu, %lu\n", buf[0].iov_len, buf[1].iov_len,
 			buf[2].iov_len );
-	writev( STDOUT_FILENO, buf, 3 );
+	printf( "len: %d\n", n );
+
+	writev( STDOUT_FILENO, buf, IOVEC_CNT );
 	printf( "\n" );
-	printf( "write: %lu, %lu, %lu\n", buf[0].iov_len, buf[1].iov_len,
-			buf[2].iov_len );
+
+	reset_buf_len( buf, IOVEC_CNT, BUF_LEN );
+
+	del_buffer( buf, IOVEC_CNT );
 
 	// read( fd, asdf, 20 );
 	// asdf[20] = 0;
